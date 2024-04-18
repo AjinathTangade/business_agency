5e84eefe38a1ed4d2a3ae639e64f687243e11777
Ajinath Tangade <ajinathtangade12@gmail.com>
2024-04-18 12:53:04 +0530

    start working on the header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-      <Button>Click me</Button>
+    <div className="container max-w-screen-xl mx-auto lg:py-4">
+    <Button>Click me</Button>
+      <h1 className="font-bold text-3xl">hello</h1>
+    </div>
+      
     </>
   )
 }
